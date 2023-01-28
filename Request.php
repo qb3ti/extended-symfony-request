e736ac6845af5b3ae6336b22a6f0b70d68148c32
Srdjan Borisavljevic <srdjan@debian.qb3ti-work>
2023-01-28 16:14:03 +0100

    init

---
+++ Request.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Qb3ti\ExtendedSymfonyRequest;
+
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+
+use function strlen;
+
+class Request extends HttpFoundationRequest
+{
+    protected static array $urlPatterns = [];
+
+    public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null)
+    {
+        parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
+        $this->loadParametersFromUrlPattern();
+    }
+
+    /**
+     * @param string[] $urlPatterns
+     */
+    public static function setUrlPatterns(array $urlPatterns): void
+    {
+        static::$urlPatterns = $urlPatterns;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getUrlPatterns(): array
+    {
+        return static::$urlPatterns;
+    }
+
+    protected function loadParametersFromUrlPattern()
+    {
+        if ($urlPattern = $this->getUrlPatternFromCurrentUrl()) {
+            $requestUriParts = explode("/", trim($this->getRequestUri(), "\/"));
+            $urlPatternParts = explode("/", trim($urlPattern, "\/"));
+            foreach ($urlPatternParts as $key => $value) {
+                if(strpos($value, "{{") !== false && strpos($value, "}}") !== false){
+                    $this->query->set(substr($value, 2, -2), $requestUriParts[$key]);
+                }
+            }
+        }
+    }
+
+    protected function getUrlPatternFromCurrentUrl(): ?string
+    {
+        if (count(static::$urlPatterns)){
+            $matches = [];
+            $uri = trim($this->getRequestUri(), "\/");
+            
+            foreach ($this->getUrlPatterns() as $urlPattern) {
+                $urlPattern = trim($urlPattern, "\/");
+                
+                if ($uri == $urlPattern) {
+                    return $urlPattern;
+                }
+
+                $pattern = preg_replace("{{[a-zA-Z0-9\_\-\{\}]+}}", "{{}}", $urlPattern);
+                $pattern = str_replace("{{","[a-zA-Z0-9\_\-\{\}\.]+", $pattern);
+                $pattern = str_replace("/", "\\/", $pattern);
+                $pattern = "/^" . $pattern . '$/';
+                $pattern = str_replace("}}", "", $pattern);
+                
+                if (preg_match($pattern, $urlPattern) && preg_match($pattern, $uri)) {
+                    $matches[] = $urlPattern;
+                }
+            }
+            
+            if (count($matches) > 0) {
+                return $matches[0];
+            }
+    
+            return null;
+        }
+        return null;
+    }
+}
---
+++ tests/RequestTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Qb3ti\ExtendedSymfonyRequest\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Qb3ti\ExtendedSymfonyRequest\Request;
+
+class RequestTest extends TestCase
+{
+    public function testLoadParametersFromQuery()
+    {
+        Request::setUrlPatterns([
+            "/path/test/{{id}}/{{name}}"
+        ]);
+        
+        $request = Request::create("/path/test/1/foo");
+
+        $this->assertEquals(
+            [
+                $request->query->get("id"),
+                $request->query->get("name")
+            ], [
+                1,
+                "foo"
+            ]
+        );
+    }
+}
